f98304d64b796f838388154133d9684fbd5c357b
David Gray <dgray@ncfgiving.com>
2023-10-02 14:24:30 -0400

    changed to online API

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
     b2cScopes: ["https://ncfpoc.onmicrosoft.com/0ab7ac63-15e0-471e-9616-71bb5252da5c/demo.read"],
-    webApi: "http://localhost:5000/hello"
+    webApi: "https://active-directory-b2c-javascript-nodejs-webapi.azurewebsites.net/hello"
   };
