a29f25c1aed2b16f3f32983f6fdf1ba00ff8e870
Schott Taylor <schotttaylor@gmail.com>
2023-12-26 18:13:13 -0700

    Testing embedded script 1

--- App/policies.js
+++ App/policies.js
@@ -5,13 +5,13 @@
  */
 const b2cPolicies = {
     names: {
-        signUpSignIn: "B2C_1A_SIGNUP_SIGNIN",
+        signUpSignIn: "B2C_1_susi",
         editProfile: "B2C_1_edit_profile",
         resetPassword: "B2C_1_reset_password"
     },
     authorities: {
         signUpSignIn: {
-            authority: "https://ncfpoc.b2clogin.com/ncfpoc.onmicrosoft.com/B2C_1A_SIGNUP_SIGNIN",
+            authority: "https://ncfpoc.b2clogin.com/ncfpoc.onmicrosoft.com/B2C_1_susi",
         },
         editProfile: {
             authority: "https://ncfpoc.b2clogin.com/ncfpoc.onmicrosoft.com/B2C_1_edit_profile"
