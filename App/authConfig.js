2b0dc2b7e9ebc5ca2220620cb4f6b59430325953
Kenga Derdus <88311008+kengaderdus@users.noreply.github.com>
2024-05-13 16:27:39 +0300

    Remove IDs use placeholders

--- App/authConfig.js
+++ App/authConfig.js
@@ -8,7 +8,7 @@
 
 const msalConfig = {
     auth: {
-      clientId: "2fdd06f3-7b34-49a3-a78b-0cf1dd87878e", // This is the ONLY mandatory field; everything else is optional.
+      clientId: "Enter_the_Application_Id_Here", // This is the ONLY mandatory field; everything else is optional.
       authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
       knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
       redirectUri: "http://localhost:6420", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
